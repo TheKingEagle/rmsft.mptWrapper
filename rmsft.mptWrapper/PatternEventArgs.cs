6398e4006370b9211636f0152e5257eb06c6ff67
rmsoft1 <rmsoftware1@gmail.com>
2023-04-02 09:38:26 -0400

    Add row and subsong to eventargs

--- rmsft.mptWrapper/PatternEventArgs.cs
+++ rmsft.mptWrapper/PatternEventArgs.cs
@@ -4,11 +4,15 @@
     {
         public int Pattern { get; private set; }
         public int Order { get; private set; }
+        public int Row { get; private set; }
+        public int SubSong { get; private set; }
 
-        public PatternEventArgs(int p, int o)
+        public PatternEventArgs(int p, int o,int r, int s)
         {
             Pattern = p;
             Order = o;
+            Row = r;
+            SubSong = s;
         }
     }
 }
